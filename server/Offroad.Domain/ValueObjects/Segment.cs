bfde014eda3a4cc8310db7e66006faf9b3f09f7b
Mikuláš Čajánek <mcajanek@email.cz>
2026-01-10 14:02:45 +0100

    #IMPLEMENTATION - segment was updated with some needed properties

--- server/Offroad.Domain/ValueObjects/Segment.cs
+++ server/Offroad.Domain/ValueObjects/Segment.cs
@@ -6,23 +6,29 @@
         public Coordinate End { get; }
         public double DistanceMeters { get; }
         public double DurationSeconds { get; }
+        public double OffroadDistanceMeters { get; }
+        public double ElevationGainMeters { get; }
+
         public IReadOnlyList<Coordinate> Geometry { get; }
 
+        //to ef core in the future
         private Segment() { }
 
-        public Segment(Coordinate start, Coordinate end, double distance, double duration, List<Coordinate> geometry)
+        public Segment(Coordinate start, Coordinate end, double distance, double duration, double offroadDistance, double elevationGain , IReadOnlyList<Coordinate> geometry)
         {
             Start = start;
             End = end;
             DistanceMeters = distance;
             DurationSeconds = duration;
+            OffroadDistanceMeters = offroadDistance;
             Geometry = geometry;
+            ElevationGainMeters = elevationGain;
         }
 
-        public static Segment Create(Coordinate start, Coordinate end, double distance, double duration, List<Coordinate> geometry)
+        public static Segment Create(Coordinate start, Coordinate end, double distance, double duration, double offroadDistance, double elevationGain, IReadOnlyList<Coordinate> geometry)
         {
             if (distance < 0) throw new ArgumentException("Distance cannot be negative");
-            return new Segment(start, end, distance, duration, geometry);
+            return new Segment(start, end, distance, duration, offroadDistance, elevationGain, geometry);
         }
     }
 }
