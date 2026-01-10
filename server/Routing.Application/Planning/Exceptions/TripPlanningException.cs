c9822d82e12a9ed5d0a57cda5e5a67d22bb1e429
Mikuláš Čajánek <mcajanek@email.cz>
2026-01-10 14:11:03 +0100

    #ADD - added trip planning exception class

---
+++ server/Routing.Application/Planning/Exceptions/TripPlanningException.cs
@@ -0,0 +1,10 @@
+﻿namespace Routing.Application.Planning.Exceptions
+{
+    public sealed class TripPlanningException : Exception
+    {
+        public TripPlanningException(string message) : base(message)
+        {
+            //todo add some type and entirely handle exceptions :)
+        }   
+    }
+}
